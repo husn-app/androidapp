a18ea79e1c6b364655911dbdab1f136306774184
Gautam Jha <gauti.jha37@gmail.com>
2024-10-23 15:25:43 +0530

    release 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.husn.fashionapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.5"
+        versionCode = 8
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
